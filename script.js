7890d22ea5839bf802bbbea9dfd1d1efff7065b7
trekin91 <31887259+trekin91@users.noreply.github.com>
2024-01-11 15:40:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
 let cube = document.getElementById("cube");
 let gameArea = document.getElementById("gameArea");
 
-function getTouchPos(touchEvent) {
-    if (touchEvent.touches) {
-        if (touchEvent.touches.length > 0) {
-            let x = touchEvent.touches[0].clientX;
-            let y = touchEvent.touches[0].clientY;
-            return { x, y };
-        }
-    }
-    return null;
+function moveCube(event) {
+    event.preventDefault();
+    let touchLocation = event.targetTouches[0];
+    
+    let gameAreaRect = gameArea.getBoundingClientRect();
+    let newX = touchLocation.clientX - gameAreaRect.left - cube.offsetWidth / 2;
+    let newY = touchLocation.clientY - gameAreaRect.top - cube.offsetHeight / 2;
+
+    if (newX < 0) newX = 0;
+    if (newY < 0) newY = 0;
+    if (newX > gameArea.offsetWidth - cube.offsetWidth) newX = gameArea.offsetWidth - cube.offsetWidth;
+    if (newY > gameArea.offsetHeight - cube.offsetHeight) newY = gameArea.offsetHeight - cube.offsetHeight;
+
+    cube.style.left = `${newX}px`;
+    cube.style.top = `${newY}px`;
 }
 
-function updatePosition(touchEvent) {
-    let touchPos = getTouchPos(touchEvent);
-    if (touchPos) {
-        let gameAreaRect = gameArea.getBoundingClientRect();
-        let newX = touchPos.x - gameAreaRect.left - cube.offsetWidth / 2;
-        let newY = touchPos.y - gameAreaRect.top - cube.offsetHeight / 2;
-
-        if (newX < 0) newX = 0;
-        if (newY < 0) newY = 0;
-        if (newX > gameArea.offsetWidth - cube.offsetWidth) newX = gameArea.offsetWidth - cube.offsetWidth;
-        if (newY > gameArea.offsetHeight - cube.offsetHeight) newY = gameArea.offsetHeight - cube.offsetHeight;
-
-        cube.style.left = newX + 'px';
-        cube.style.top = newY + 'px';
-    }
-}
-
-gameArea.addEventListener('touchstart', updatePosition);
-gameArea.addEventListener('touchmove', function(e) {
-    updatePosition(e);
-    e.preventDefault(); // Empêche le scrolling par défaut lors du toucher
-});
+gameArea.addEventListener('touchmove', moveCube);
